1e6c0b629f008e71b789b4c86a2d81ef90d4be6d
Kaian Jiang <rean@Arch.localdomain>
2021-10-22 08:41:41 +0800

    Oct 22th, 2021

--- try.py
+++ try.py
@@ -4,3 +4,15 @@ print("hello")
 base1 = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"
 print(len(binascii.a2b_base64(base1).decode()))
 print(binascii.a2b_base64(base1))
+
+from Crypto.Cipher import AES
+
+key = b'Sixteen byte key'
+data = b'hello from other side'
+cipher = AES.new(key, AES.MODE_EAX)
+
+e_data = cipher.encrypt(data)
+d_data = cipher.decrypt(e_data)
+
+print("Encryption was: ", e_data)
+print("Original Message was: ", d_data)
